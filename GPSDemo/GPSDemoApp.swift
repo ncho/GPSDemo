4f86260b89986f6f81af7eaf95f3974885e18b49
Nathan Cho <ncho@esri.com>
2025-08-06 12:11:47 -0700

    Initial Commit

---
+++ GPSDemo/GPSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSDemoApp.swift
+//  GPSDemo
+//
+//  Created by Nathan Cho on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GPSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GPSDemoTests/GPSDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GPSDemoTests.swift
+//  GPSDemoTests
+//
+//  Created by Nathan Cho on 8/6/25.
+//
+
+import Testing
+@testable import GPSDemo
+
+struct GPSDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
